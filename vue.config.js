0da1dd8a3337ca2a7f408632febed201f7100f71
naoki yoshizawa <yoshizawa@nrsd.jp>
2022-06-13 09:29:03 +0900

    pwa対応強化

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
+const BASE_PATH = process.env.NODE_ENV === 'production' ? '/mj-score-board/' : '/'
+
 module.exports = defineConfig({
   pages: {
     index: {
@@ -6,8 +8,39 @@ module.exports = defineConfig({
       title: '麻雀スコアボード',
     }
   },
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/mj-score-board/'
-    : '/',
+  publicPath: BASE_PATH,
+  pwa: {
+    name: '麻雀スコアボード',
+    themeColor: '#deb887',
+    appleMobileWebAppCapable: 'yes',
+    manifestOptions: {
+      short_name: 'すこあぼ',
+      start_url: BASE_PATH,
+      display: 'fullscreen',
+      description: '麻雀のスコアを記録するウェブアプリ',
+      orientation: 'any',
+      background_color: '#deb887',
+      icons: [
+        {
+          src: 'favicon/android-chrome-192x192.png',
+          size: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'favicon/android-chrome-512x512.png',
+          size: '512x512',
+          type: 'image/png',
+        },
+      ]
+    },
+    iconPaths: {
+      faviconSVG: null,
+      favicon16: 'favicon/favicon-16x16.png',
+      favicon32: 'favicon/favicon-32x32.png',
+      appleTouchIcon: 'favicon/apple-touch-icon.png',
+      maskIcon: null,
+      msTileImage: null,
+    },
+  },
   transpileDependencies: true,
 })
